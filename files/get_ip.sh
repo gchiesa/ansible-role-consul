9904448b9bce3685b8c0b4e6dfc11547b0c9638a
Giuseppe Chiesa <giuseppe.chiesa@oracle.com>
2016-12-14 09:40:49 +0100

    improved host ip detection

--- files/get_ip.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-data=$(grep $1 /etc/hosts)
-if [[ $? == 0 ]]; then
-  ip=$(echo "$data"|tail -1| cut -d' ' -f1)
-else
-  ip=$(host $1 | awk '/has address/ { print $4 }')
-fi
-echo "$ip"
